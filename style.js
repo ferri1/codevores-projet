bb536fde9fb43fe614d039a12ca84c7de488e548
Ferri19 <lyndaferri07@gmail.com>
2020-03-27 19:22:37 +0100

    modif

--- style.js
+++ style.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
   $('.acc h3').click(function(){
     $(this).next('.content').slideToggle();
-    $(this).parent()addClass('acti');
-    $(this).parent()siblings()children('.content').slideUp();
-    $(this).parent()siblings()removeClass('acti');
+    $(this).parent().addClass('acti');
+    $(this).parent().siblings().children('.content');
+    $(this).parent().siblings().removeClass('acti');
   });
 });
