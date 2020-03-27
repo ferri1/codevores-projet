b46de96b6b5b57e8239d7ca5fd4dcabcbc416ae3
Ferri19 <lyndaferri07@gmail.com>
2020-03-27 19:02:47 +0100

    des modification
    j'ai changé ma page index
    j'ai commencé ma page faq

--- script.js
+++ script.js
@@ -21,10 +21,10 @@ document.querySelector('.chat').addEventListener('click', function() {
   changerTheme('theme-nuit');
 });
 
-document.querySelector("#envoyer").addEventListener("click", function() {
+document.querySelector("#envoyer").addEventListener('click', function() {
   console.log("ça fonctionne à partir du document.querySelector('#envoyer')");
-})
+});
 
 envoyer.addEventListener("click", function() {
   console.log("ça fonctionne à partir de l'objet créer par le navigateur à partir de l'ID de l'élément HTML");
-})
+});
