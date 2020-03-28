1797af42750cbdae43b0135fbfb8e89d36e09a07
Ferri19 <lyndaferri07@gmail.com>
2020-03-29 00:45:59 +0100

    modification
    finir page faq et inscription
    et faire quelque modification sur queqlue pages

--- style.js
+++ style.js
@@ -1,8 +1,5 @@
-$(document).ready(function(){
-  $('.acc h3').click(function(){
-    $(this).next('.content').slideToggle();
-    $(this).parent().addClass('acti');
-    $(this).parent().siblings().children('.content');
-    $(this).parent().siblings().removeClass('acti');
-  });
+
+
+$(':radio').change(function() {
+ console.log('New star rating: ' + this.value);
 });
