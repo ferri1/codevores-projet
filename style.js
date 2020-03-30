da07bc523b191a8674b24f1a1199bc40f7d50f28
Ferri19 <lyndaferri07@gmail.com>
2020-03-30 23:19:33 +0200

    99% de projet est finis

--- style.js
+++ style.js
@@ -1,12 +1,20 @@
-// $(document).ready(function(){
-//   $('.acc h3').click(function(){
-//     $(this).next('.content').slideToggle();
-//     $(this).parent().addClass('acti');
-//     $(this).parent().siblings().children('.content');
-//     $(this).parent().siblings().removeClass('acti');
-//   });
+$(document).ready(function(){
+  $('.acc h3').click(function(){
+    $(this).next('.content').slideToggle();
+    $(this).parent().addClass('acti');
+    $(this).parent().siblings().children('.content');
+    $(this).parent().siblings().removeClass('acti');
+   });
+});
+
+$('.radio').change(function() {
+ console.log('New star rating: ' + this.value);
+});
 
 
-// $('.radio').change(function() {
-//  console.log('New star rating: ' + this.value);
-// });
+function bascule(id)
+{
+if (document.getElementById(id).style.visibility == "hidden")
+document.getElementById(id).style.visibility = "visible";
+else	document.getElementById(id).style.visibility = "hidden";
+}
